58ece6acb879e2c62e16a3885babf57c6448cd1d
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-05-29 10:55:38 -0300

    Alterado os Tipos dos arquivos, para poder facilitar na implementação, mudado para o php bruto. Para poder usar comandos como include e para preparar para modificações futuras.

---
+++ produtos.php
@@ -0,0 +1,22 @@
+<?php
+include('topo.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="petsho.css">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script> 
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <link type="image/png" sizes="16x16" rel="icon" href="PetShop/img/favicon.svg">
+    <title>Produtos</title>
+</head>
+<body>
+    <h1 class=" informa">Pagina em Construção</h1>
+</body>
+</html>
---
+++ servicos.php
@@ -0,0 +1,56 @@
+<?php
+include('topo.php')
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="petsho.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link type="image/png" sizes="16x16" rel="icon" href="PetShop/img/favicon.svg">
+    <title>Serviços</title>
+</head>
+<body>
+
+<!-- Início da lista de cards -->
+<div class="cards">
+    <div class="cards-list"> 
+       <div class="cards">
+        <h2>Nossos Serviços</h2>
+        <div class="cards-list">
+            <!-- Card 1 -->
+            <div class="card-item">
+                <img src="img/banho-dog.jpg" alt="Banho e Tosa">
+                <h3>Banho e Tosa</h3>
+                <p class="Descricao">Serviço completo de banho e tosa para deixar seu pet cheiroso e feliz.</p>
+            </div>
+
+            <!-- Card 2 -->
+            <div class="card-item">
+                <img src="img/tosa-bebe.jpg" alt="Tosa">
+                <h3>Tosa Higienica</h3>
+                <p class="Descricao">Atendimento veterinário com profissionais qualificados e experientes.</p>
+            </div>
+
+            <!-- Card 3 -->
+            <div class="card-item">
+                <img src="img/hidratacao.png" alt="Mascara de hidratacao Para Cachorro">
+                <h3>Mascara de hidratacao</h3>
+                <p class="Descricao">Mascara de hidratacao Para seu Cachorro Ficar Com o Pelo Brilhando.</p>
+            </div>
+
+              <!-- Card 4 -->
+            <div class="card-item">
+                <img src="img/hotel-dog.png" alt="Mascara de hidratacao Para Cachorro">
+                <h3>Hotel para Pets</h3>
+                <p class="Descricao">Seu pet Traga-o para nosso Hotel para Pets e viaje tranquilo, sabendo que ele estará em boas mãos! .</p>
+            </div>
+    </div> <!-- Fim da lista de cards -->
+</div>
+</body>
+</html>
